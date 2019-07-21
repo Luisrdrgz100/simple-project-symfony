965eb9ecd925b2a759cecb8150eb2cb29daef90b
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-07-21 21:45:17 +0200

    table factura created

---
+++ src/AppBundle/Entity/Factura.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Factura
+ *
+ * @ORM\Table(name="factura")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FacturaRepository")
+ */
+class Factura
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="importe", type="integer")
+     */
+    private $importe;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_cliente", type="integer")
+     */
+    private $idCliente;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return Factura
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set importe
+     *
+     * @param integer $importe
+     *
+     * @return Factura
+     */
+    public function setImporte($importe)
+    {
+        $this->importe = $importe;
+
+        return $this;
+    }
+
+    /**
+     * Get importe
+     *
+     * @return int
+     */
+    public function getImporte()
+    {
+        return $this->importe;
+    }
+
+    /**
+     * Set idCliente
+     *
+     * @param integer $idCliente
+     *
+     * @return Factura
+     */
+    public function setIdCliente($idCliente)
+    {
+        $this->idCliente = $idCliente;
+
+        return $this;
+    }
+
+    /**
+     * Get idCliente
+     *
+     * @return int
+     */
+    public function getIdCliente()
+    {
+        return $this->idCliente;
+    }
+}
+
