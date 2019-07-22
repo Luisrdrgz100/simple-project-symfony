ceeed194958842af83cae34ea09b1e1c597fcc75
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-07-22 15:14:12 +0200

    automatic crud client created

--- src/AppBundle/Controller/ClienteController.php
+++ src/AppBundle/Controller/ClienteController.php
@@ -2,43 +2,135 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Cliente;
-use AppBundle\Forms\ClienteType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
-
-
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
 /**
- * @Route("/clientes")
+ * Cliente controller.
+ *
+ * @Route("clientes")
  */
-
 class ClienteController extends Controller
 {
     /**
-     * @Route("/new", name="new")
+     * Lists all cliente entities.
+     *
+     * @Route("/", name="clientes_index")
+     * @Method("GET")
      */
-    public function newClienteAction(Request $request)
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $clientes = $em->getRepository('AppBundle:Cliente')->findAll();
+
+        return $this->render('cliente/index.html.twig', array(
+            'clientes' => $clientes,
+        ));
+    }
+
+    /**
+     * Creates a new cliente entity.
+     *
+     * @Route("/new", name="clientes_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
     {
         $cliente = new Cliente();
-
-        $form = $this->createForm(ClienteType::class, $cliente);
-
+        $form = $this->createForm('AppBundle\Form\ClienteType', $cliente);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($cliente);
+            $em->flush();
 
-            $cliente = $form->getData();
-
-            $save = $this->getDoctrine()->getManager();
-            $save->persist($cliente);
-            $save->flush();
-
-            return $this->redirectToRoute('clientes');
+            return $this->redirectToRoute('clientes_show', array('id' => $cliente->getId()));
         }
-        return $this->render('cliente/newCliente.html.twig', array('form' => $form->createView()));
+
+        return $this->render('cliente/new.html.twig', array(
+            'cliente' => $cliente,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a cliente entity.
+     *
+     * @Route("/{id}", name="clientes_show")
+     * @Method("GET")
+     */
+    public function showAction(Cliente $cliente)
+    {
+        $deleteForm = $this->createDeleteForm($cliente);
+
+        return $this->render('cliente/show.html.twig', array(
+            'cliente' => $cliente,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing cliente entity.
+     *
+     * @Route("/{id}/edit", name="clientes_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Cliente $cliente)
+    {
+        $deleteForm = $this->createDeleteForm($cliente);
+        $editForm = $this->createForm('AppBundle\Form\ClienteType', $cliente);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('clientes_edit', array('id' => $cliente->getId()));
+        }
+
+        return $this->render('cliente/edit.html.twig', array(
+            'cliente' => $cliente,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a cliente entity.
+     *
+     * @Route("/{id}", name="clientes_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Cliente $cliente)
+    {
+        $form = $this->createDeleteForm($cliente);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($cliente);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('clientes_index');
+    }
+
+    /**
+     * Creates a form to delete a cliente entity.
+     *
+     * @param Cliente $cliente The cliente entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Cliente $cliente)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('clientes_delete', array('id' => $cliente->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
